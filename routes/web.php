bd34e8e971668f34a7edc30ba9faff65a783ed8b
jorge <jorge.ramirez.br@gmail.com>
2023-09-26 14:07:53 +0200

    manage dashboard views

--- routes/web.php
+++ routes/web.php
@@ -14,15 +14,25 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
-Route::middleware([
+/* Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
     'verified',
 ])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+    Route::get('/dash', function () {
+        return view('dash.index');
+    })->name('dash');
+}); */
+
+Route::get('/dash', 'App\Http\Controllers\DashboardController@index');
+
+Route::get('/dash/crud', function () {
+    return view('crud.index');
+});
+
+Route::get('/dash/crud/create', function () {
+    return view('crud.create');
 });
